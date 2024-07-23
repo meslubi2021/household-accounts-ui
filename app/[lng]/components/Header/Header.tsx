084c73833d3636d22dd492560b2205c672e69e1f
Jehyun Jung <congmul@hotmail.com>
2024-07-23 16:02:01 -0700

    Update colors

--- app/[lng]/components/Header/Header.tsx
+++ app/[lng]/components/Header/Header.tsx
@@ -1,26 +1,21 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import { useRouter, usePathname, useSearchParams } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 import { useTranslation } from '../../../i18n/client'
 
 export const Header = ({ lng }: { lng: string }) => {
-  const router = useRouter();
   const pathname = usePathname();
-  const searchParams = useSearchParams();
   const { t } = useTranslation(lng, 'main');
   const [ pageName, setPageName ] = useState("calendar");
 
   useEffect(() => {
-    console.log(router);
-    const params = searchParams.toString();
-    const fullUrl = `${window.location.origin}${pathname}${params ? `?${params}` : ''}`;
     const pathNameArr = pathname.split('/');
     setPageName(pathNameArr[pathNameArr.length - 1]);
-  }, [pathname, searchParams]);
+  }, [pathname]);
 
   return (
-    <div className="header flex justify-around items-center">
+    <div className="header flex justify-around items-center bg-red-3 text-white">
       {t(`${pageName}.header.title`)}
   </div>)
 }
