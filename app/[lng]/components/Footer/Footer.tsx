bf37d53dbb898936f4f598878588428785fd9539
Jehyun Jung <congmul@hotmail.com>
2024-07-22 22:49:49 -0700

    Update style

--- app/[lng]/components/Footer/Footer.tsx
+++ app/[lng]/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ import { CalendarDaysIcon, ChartPieIcon, PlusCircleIcon, CurrencyDollarIcon, Cog
 export const Footer = async ({ lng }: { lng: string }) => {
     const { t } = await useTranslation(lng, 'footer');
     return (
-      <footer className="h-1/6 flex justify-around items-center">
+      <footer className="flex justify-around items-center">
         <Link href="/calendar" className="flex flex-col items-center">
           <CalendarDaysIcon />
           <span className="text-gray-500">{t(`calendar`)}</span>
