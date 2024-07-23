a480acc719d905c68dcae78b93b17f2875e34e7e
Jehyun Jung <congmul@hotmail.com>
2024-07-23 07:43:42 -0700

    Refine i18n behavior

--- app/[lng]/components/Header/Header.tsx
+++ app/[lng]/components/Header/Header.tsx
@@ -1,9 +1,26 @@
-import { useTranslation } from '../../../i18n'
+'use client';
 
-export const Header = async ({ lng }: { lng: string }) => {
-    const { t } = await useTranslation(lng, 'footer');
-    return (
-      <div className="header flex justify-around items-center">
-        Header
-    </div>)
+import { useState, useEffect } from 'react';
+import { useRouter, usePathname, useSearchParams } from 'next/navigation';
+import { useTranslation } from '../../../i18n/client'
+
+export const Header = ({ lng }: { lng: string }) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const { t } = useTranslation(lng, 'main');
+  const [ pageName, setPageName ] = useState("calendar");
+
+  useEffect(() => {
+    console.log(router);
+    const params = searchParams.toString();
+    const fullUrl = `${window.location.origin}${pathname}${params ? `?${params}` : ''}`;
+    const pathNameArr = pathname.split('/');
+    setPageName(pathNameArr[pathNameArr.length - 1]);
+  }, [pathname, searchParams]);
+
+  return (
+    <div className="header flex justify-around items-center">
+      {t(`${pageName}.header.title`)}
+  </div>)
 }
