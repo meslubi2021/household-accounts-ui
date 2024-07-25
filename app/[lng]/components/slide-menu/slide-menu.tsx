130131975ae671b6adb5c2c194fe4a7b58185287
Jehyun Jung <congmul@hotmail.com>
2024-07-24 19:09:23 -0700

    Add Form

--- app/[lng]/components/slide-menu/slide-menu.tsx
+++ app/[lng]/components/slide-menu/slide-menu.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from 'react';
 import { useTranslation } from '../../../i18n/client'
+import { format, isToday,  parseISO } from "date-fns"
 
 interface SlideMenuType {
     isOpen: boolean,
@@ -11,6 +12,18 @@ interface SlideMenuType {
 
 const SlideMenu: React.FC<SlideMenuType> = ({isOpen, close, lng}) => {
     const { t } = useTranslation(lng, 'main');
+    const [activeTab, setActiveTab] = useState('expense');
+    const [date, setDate] = useState<string>(format(new Date(), 'yyyy-MM-dd'));
+    const [amount, setAmount] = useState<string>('');
+    const [category, setCategory] = useState<string>('food');
+    const [note, setNote] = useState<string>('');
+
+    const handleAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        // const formattedValue = parseFloat(value).toFixed(2);
+        setAmount(isNaN(parseFloat(value)) ? '' : value);
+    };
+
     return (
     <>
       <div
@@ -23,32 +36,80 @@ const SlideMenu: React.FC<SlideMenuType> = ({isOpen, close, lng}) => {
             <div onClick={() => close()} className="text-white p-2 px-3">
                 X
             </div>
+            <div>
+                <button
+                    className={`px-4 py-2 rounded ${activeTab === 'expense' ? 'bg-white text-red-300' : 'text-gray-500'}`}
+                    onClick={() => setActiveTab('expense')}
+                    >
+                    Expense
+                </button>
+                <button
+                    className={`px-4 py-2 rounded ${activeTab === 'income' ? 'bg-white text-red-300' : 'text-gray-500'}`}
+                    onClick={() => setActiveTab('income')}
+                    >
+                    Income
+                </button>
+            </div>
             <div onClick={() => close()} className="text-white p-2 px-3">
                 {t('slide-menu.save')}
             </div>
         </div>
         <div className='main'>
-            <ul>
-            <li className="mb-2">
-                <a href="#" className="text-blue-500 hover:underline">
-                Menu Item 1
-                </a>
-            </li>
-            <li className="mb-2">
-                <a href="#" className="text-blue-500 hover:underline">
-                Menu Item 2
-                </a>
-            </li>
-            <li className="mb-2">
-                <a href="#" className="text-blue-500 hover:underline">
-                Menu Item 3
-                </a>
-            </li>
-            </ul>
-        </div>
-        <div className='footer bg-red-300'>
+            <div className="max-w-md mx-auto bg-white overflow-hidden">
+                <div className="p-4 space-y-4">
+                    <div className="flex justify-between items-center border-b">
+                        <span className="flex items-center">
+                            <span className="mr-2">Date</span>
+                            {isToday(parseISO(date)) && (
+                                <span className="ml-2 border text-blue text-xs px-2 rounded">Today</span>
+                            )}
 
+                        </span>
+                        <input
+                            type="date"
+                            value={date}
+                            onChange={(e) => setDate(e.target.value)}
+                            className="text-left w-2/3 px-2 py-1"
+                        />
+                    </div>
+                    <div className="flex justify-between items-center border-b">
+                        <span>Amount</span>
+                        <span className="text-left w-2/3 px-2 py-1">
+                            <span className="mr-2">$</span>
+                            <input
+                                type="number"
+                                value={amount}
+                                onChange={handleAmountChange}                                
+                                placeholder="0.00"
+                                min="0"
+                            />
+                        </span>
+                    </div>
+                    <div className="flex justify-between items-center border-b">
+                        <span>Category</span>
+                        <select
+                            value={category}
+                            onChange={(e) => setCategory(e.target.value)}
+                            className="text-left w-2/3 px-2 py-1"
+                        >
+                            <option value="food">🍴 Food</option>
+                            <option value="transport">🚗 Transport</option>
+                            <option value="shopping">🛍️ Shopping</option>
+                            <option value="entertainment">🎉 Entertainment</option>
+                        </select>
+                    </div>
+                    <div className="border-b w-100">
+                        <textarea
+                            value={note}
+                            onChange={(e) => setNote(e.target.value)}
+                            className="w-[100%] px-2 py-1"
+                            placeholder="Add a note"
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
+        <div className='footer bg-red-300'></div>
       </div>
     </>
   );
