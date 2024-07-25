f6d8595e2a7a27f89ac8332a948c8c5cb1ff83f2
Jehyun Jung <congmul@hotmail.com>
2024-07-25 14:07:50 -0700

    Add ideas on settins

--- app/[lng]/settings/page.tsx
+++ app/[lng]/settings/page.tsx
@@ -6,21 +6,31 @@ import { useTranslation } from '../../i18n';
 export default async function Index({ params: { lng }} : any) {
     const { t } = await useTranslation(lng, 'main')    
 
-    return (<>
+    return (<div className="p-3">
        settings
-        <Trans i18nKey="languageSwitcher" t={t}>
-            Switch from <strong>{lng}</strong> to:{' '}
-        </Trans>
-        {languages.filter((l) => lng !== l).map((l, index) => {
-            return (
-            <span key={l}>
-                {index > 0 && (' or ')}
-                <Link href={`/${l}/settings`}>
-                {l}
-                </Link>
-            </span>
-            )
-        })}
-    </>
+       <ul>
+        <li>
+            <Trans i18nKey="languageSwitcher" t={t}>
+                Switch from <strong>{lng}</strong> to:{' '}
+            </Trans>
+            {languages.filter((l) => lng !== l).map((l, index) => {
+                return (
+                <u key={l}>
+                    {index > 0 && (' or ')}
+                    <Link href={`/${l}/settings`}>
+                    {l}
+                    </Link>
+                </u>
+                )
+            })}
+        </li>
+        <li>
+            Manage Categories
+        </li>
+        <li>
+            Manage Fixed Expenses
+        </li>
+       </ul>        
+    </div>
     );
 }
