e0f8f7927274d27c87e8008cb112a446bb033b36
Jehyun Jung <congmul@hotmail.com>
2024-07-27 12:11:01 -0700

    Create Reducer

---
+++ app/[lng]/components/add-new-item/utils/action.ts
@@ -0,0 +1,15 @@
+export enum HandleItemAction {
+    DATE = 'DATE',
+    AMOUNT = 'AMOUNT',
+    CATEGORIES = 'CATEGORIES',
+    CATEGORY = 'CATEGORY',
+    NOTE = 'NOTE',
+    IS_SAVING = 'IS_SAVING',
+    IS_ABLE_TO_SAVE = 'IS_ABLE_TO_SAVE',
+    RESET = 'RESET'
+}
+
+export enum HandleItemAlertAction {
+
+}
+
