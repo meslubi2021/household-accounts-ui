5c3728c7fc4978e3a44a5994aa146d2d3f6d6345
Jehyun Jung <congmul@hotmail.com>
2024-08-01 20:53:08 -0700

    updated

--- app/[lng]/api-services/budget.service.ts
+++ app/[lng]/api-services/budget.service.ts
@@ -4,45 +4,10 @@ import { Budget } from '../models';
 export const budgetService = {
     getByUserIdMonth: async (userId: string, budgetMonth: string):Promise<Budget | undefined> => {
         try{
-            // const { data } = await axios.get(`localhost.../${userId}/${budgetMonth}`);
-            // return data;
-            return mockData[budgetMonth];
+            const { data } = await axios.get(`localhost.../${userId}/${budgetMonth}`);
+            return data;            
         }catch(err){
 
         }
     }
 }
-
-const mockData:any = 
-    {
-        "2024-07":  {
-            userId: 'user-id',
-            budgetMonth: '2024-07',
-            totalAmount: 6723.67,
-            items: [
-                {
-                    source: "skillstorm",
-                    amount: 3223.12
-                },
-                {
-                    source: "impexGLS",
-                    amount: 3500.52
-                }
-            ]
-        },
-        "2024-06":  {
-            userId: 'user-id',
-            budgetMonth: '2024-07',
-            totalAmount: 6245.52,
-            items: [
-                {
-                    source: "skillstorm",
-                    amount: 2900.00
-                },
-                {
-                    source: "impexGLS",
-                    amount: 3345.52
-                }
-            ]
-        }
-    }
