193ab918fbcc37acbedbe9caa9f0195debcc4aed
Jehyun Jung <congmul@hotmail.com>
2024-08-01 22:37:09 -0700

    Update

--- app/[lng]/components/shared/swipeable-card/swepeable-card.tsx
+++ app/[lng]/components/shared/swipeable-card/swepeable-card.tsx
@@ -4,6 +4,9 @@ import { useState } from 'react';
 import { motion, useMotionValue, useTransform } from "framer-motion"
 import { PencilSquareIcon, TrashIcon } from '@heroicons/react/24/outline'
 import { TransactionType } from '../../../models';
+import { formatCurrency } from '@/app/[lng]/utils';
+import { transactionService } from '../../../api-services';
+import { LoadingSpinner } from '..';
 
 interface SwipeableCardType{
     transaction: {
@@ -20,6 +23,7 @@ interface SwipeableCardType{
 
 export const SwipeableCard:React.FC<SwipeableCardType> = ({transaction, editOnClick}) => {
     const [isDragging, setIsDragging] = useState(false);
+    const [ isSaving, setIsSaving ] = useState(false);
     const x = useMotionValue(0);
     const boxShadow = useTransform(
         x,
@@ -44,6 +48,19 @@ export const SwipeableCard:React.FC<SwipeableCardType> = ({transaction, editOnCl
             e.stopPropagation();
         }else{
             callback(e);
+        }
+    }
+    async function handleDelete(e:any){
+        try{
+            e.stopPropagation();
+            setIsSaving(true);
+            const res = await transactionService.deleteTransaction(e.currentTarget.dataset.transactionId);
+        }catch(err){
+            console.log(err);
+        }finally{
+            setTimeout(() => {
+                setIsSaving(false);
+            }, 1000);
         }
     }
 
@@ -83,7 +100,7 @@ export const SwipeableCard:React.FC<SwipeableCardType> = ({transaction, editOnCl
                 <div className="ml-2  text-gray-500">{transaction.note}</div>
             </div>
             <div className={'text-red-500  font-bold'}>
-                -${transaction.amount}
+                -${formatCurrency(transaction.amount)}
             </div>
         </motion.div>
         <motion.div className="actions absolute top-0 right-0 p-2 flex overflow-hidden" style={{ opacity, width }}>                
@@ -95,12 +112,12 @@ export const SwipeableCard:React.FC<SwipeableCardType> = ({transaction, editOnCl
                 data-note={transaction.note}
                 onClick={editOnClick}
             >
-                <PencilSquareIcon width={"18px"} />
+                <PencilSquareIcon className="pb-0.5" width={"18px"} />
                 <span>Edit</span>
             </motion.button>
-            <motion.button className="px-2 flex flex-col items-center overflow-hidden text-red-500 hover:text-red-600" style={{ width: childWidth }}>
-                <TrashIcon width={"18px"}/>
-                <span>Delete</span>
+            <motion.button onClick={handleDelete} data-transaction-id={transaction._id} className="px-2 flex flex-col items-center overflow-hidden text-red-500 hover:text-red-600" style={{ width: childWidth }}>                
+                {!isSaving ? <TrashIcon className="pb-0.5" width={"18px"}/> : <LoadingSpinner />}
+                <span>Delete</span> 
             </motion.button>
         </motion.div>
     </motion.div>
