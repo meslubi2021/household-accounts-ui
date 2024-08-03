da5c31dc2930e203cbbee3506a502e91e86e59ee
Jehyun Jung <congmul@hotmail.com>
2024-08-02 17:40:37 -0700

    login page

--- app/[lng]/login/layout.tsx
+++ app/[lng]/login/layout.tsx
@@ -1,5 +1,30 @@
-export const metadata = {
-  title: 'Household Accounts Login'
+import type { Metadata, Viewport } from "next";
+import "../sass/index.scss";
+import { dongle } from "../utils";
+
+export const metadata: Metadata = {
+  title: "Household Account",
+  description: "Mange household account.",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["nextjs", "next14", "pwa", "next-pwa"],
+  // themeColor: [{ media: "(prefers-color-scheme: light)", color: "#fff" }],
+  authors: [
+    {
+      name: "Jehyun Jung",
+      url: "https://www.linkedin.com/in/jehyun-jung-bb247813b/",
+    },
+  ],
+  // viewport:
+    // "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  icons: [
+    { rel: "apple-touch-icon", url: "assets/icons/icon-128x128.png" },
+    { rel: "icon", url: "assets/icons/icon-128x128.png" },
+  ],
+};
+
+export const viewport: Viewport = {
+  width:"device-width", initialScale: 1, interactiveWidget: "resizes-content"
 }
 
 export default function RootLayout({
--- app/[lng]/login/page.tsx
+++ app/[lng]/login/page.tsx
@@ -3,51 +3,56 @@
 import { useTranslation } from '../../i18n/client';
 import React, { useState, useEffect } from 'react';
 // import { useRouter } from 'next/router';
+import { MSLoginButton, GoogleLoginButton } from '../components/buttons';
 
 export default function Index({ params: { lng }} : any) {
     const { t } = useTranslation(lng, 'main');
+    const [email, setEmail] = useState('');
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    // const router = useRouter();
     
     const handleSubmit = async (e:any) => {
         e.preventDefault();
-    
-        // const res = await fetch('/api/login', {
-        //   method: 'POST',
-        //   headers: {
-        //     'Content-Type': 'application/json'
-        //   },
-        //   body: JSON.stringify({ username, password })
-        // });
-    
-        // if (res.ok) {
-        //   router.push('/protected');
-        // } else {
-        //   console.error('Failed to login');
-        // }
     };
 
     return (<>
        
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          placeholder="Username"
-          required
-        />
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-          required
-        />
-        <button type="submit">Login</button>
-      </form>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-md shadow-md">
+        <h2 className="text-2xl font-bold text-center">Login</h2>
+        <form className="space-y-4" onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+              Mobile number or email address:
+            </label>
+            <input
+              type="text"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              className="block w-full px-3 py-2 mt-1 text-gray-700 border rounded-md shadow-sm focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+            />
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="block w-full px-4 py-2 text-sm font-medium text-white bg-black rounded-md hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              CONTINUE
+            </button>
+          </div>
+        </form>
+        <div className="flex items-center justify-between mt-4">
+          <hr className="w-full border-gray-300" />
+          <span className="px-2 text-sm text-gray-500">Or</span>
+          <hr className="w-full border-gray-300" />
+        </div>
+        <div className="space-y-4">
+            <MSLoginButton /> 
+            <GoogleLoginButton />
+        </div>
+      </div>
     </div>
     </>
     );
