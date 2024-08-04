60a9e41bf1cf867568767cd2de47c9c9ba4862c1
Jehyun Jung <congmul@hotmail.com>
2024-08-03 22:43:38 -0700

    Update file name

--- app/[lng]/components/shared/index.ts
+++ app/[lng]/components/shared/index.ts
@@ -5,5 +5,6 @@ import { AmountInput } from './amount-Input/amount-Input';
 import { HandleItemSlideMenu } from './handle-item-slide-menu/handle-item-slide-menu';
 import { SwipeableCard } from './swipeable-card/swepeable-card';
 import { CustomInput } from './custom-input/custom-input';
+import { ToggleButton } from './toggle-button/toggle-button'
 
-export { Dropdown, LoadingSpinner, AmountInput, SlideMenu, HandleItemSlideMenu, SwipeableCard, CustomInput }
+export { Dropdown, LoadingSpinner, AmountInput, SlideMenu, HandleItemSlideMenu, SwipeableCard, CustomInput, ToggleButton }
