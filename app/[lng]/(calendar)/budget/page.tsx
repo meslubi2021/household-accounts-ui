aa22dbcf43b18d0a3ea9cc839e3f5826367989d6
Jehyun Jung <congmul@hotmail.com>
2024-08-04 14:24:53 -0700

    Basic tab menu on Budget page

--- app/[lng]/(calendar)/budget/page.tsx
+++ app/[lng]/(calendar)/budget/page.tsx
@@ -3,12 +3,15 @@
 import { useTranslation } from '../../../i18n/client';
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { transactionService } from '../../api-services';
+import { transactionService, budgetService, categoryService } from '../../api-services';
 import { useSessionStorageState } from '../../utils/custom-hook';
 import { calendarActions, refreshActions } from '../../utils/redux';
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import { format } from 'date-fns'
+import {  Tabs, Tab  } from '../../components/shared';
+import { formatCurrency } from '../../utils';
+import { Budget, Category } from '../../models';
 
 export default function Index({ params: { lng }} : any) {
     const { t } = useTranslation(lng, 'main');
@@ -17,6 +20,9 @@ export default function Index({ params: { lng }} : any) {
     const { isBudgetPageRefresh } = useSelector((state:any) => state.refresh);
     const [ userInfo, _ ] = useSessionStorageState("userInfo", "");
     const [ totalIncome, setTotalIncome ] = useState(0);
+    const [ budget, setBudget ] = useState<Budget>();
+    const [ expenseCategories, setExpenseCategories ] = useState<Category[]>();
+    const [ activeTab, setActiveTab ] = useState(0);
 
     useEffect(() => {
         if(!isBudgetPageRefresh) return;
@@ -38,6 +44,10 @@ export default function Index({ params: { lng }} : any) {
             const [year, month] = selectedDateStr.split('-'); // ["2024", "08"]
             const prevMonth = parseInt(month) - 1;
             const transactionRes = await transactionService.getIncomeByUserId(userInfo._id, year, prevMonth.toString());
+            const budget = await budgetService.getByUserId(userInfo._id, year, month);
+            const categories = await categoryService.getByUserId(userInfo._id, 'expense');
+            setExpenseCategories(categories)
+            setBudget(budget);
             let totalAmount = 0;
             transactionRes?.forEach(transaction => totalAmount += transaction.totalAmount);
             setTotalIncome(totalAmount);
@@ -53,6 +63,14 @@ export default function Index({ params: { lng }} : any) {
       dispatch(calendarActions.setSelectedDateStr(format(currentDate, 'yyyy-MM-dd')));
     };
 
+    function calBalance(income:number, budget:number) {
+      if(income - budget < 0){
+        return <span className="text-red-500">-${formatCurrency(income - budget).split("-")[1]}</span>
+      }else{
+        return `$${formatCurrency(income - budget)}`
+      }
+    }
+
     return (<>
     <div className="budget-page-wrapper">
         <FullCalendar 
@@ -65,8 +83,37 @@ export default function Index({ params: { lng }} : any) {
             }}
          />
     </div>
-    <div>
-       Total incomes: {totalIncome}
+    <div className="flex flex-col list-of-budgets p-4 h-[39vh]">
+       <div className="flex justify-between mb-4">
+            <div className="text-center">
+                <p>{`${t('general.income')} (${t('general.last_month')})`}</p>
+                <p className="text-green-500 font-bold">${formatCurrency(totalIncome)}</p>
+            </div>
+            <div className="text-center">
+                <p>{t('general.budget')}</p>
+                <p className="text-red-500 font-bold">${formatCurrency(budget?.totalAmount || 0)}</p>
+            </div>
+            <div className="text-center">
+                <p>{t('general.balance')}</p>
+                <p className="text-blue-500 font-bold">{calBalance(totalIncome, budget?.totalAmount || 0)}</p>
+            </div>
+        </div>
+            <Tabs activeTab={activeTab} setActiveTab={setActiveTab}>
+                <Tab label={t("general.expense")}>
+                {
+                    expenseCategories && expenseCategories.length > 0
+                    ?
+                        expenseCategories.map(category => {
+                            return (<div>{category.name}</div>)
+                        })
+                    : 
+                        <div>Add new category</div>
+                }
+                </Tab>
+                <Tab label={t("general.investment")}>
+                    <div>Coming soon</div>
+                </Tab>
+            </Tabs>
     </div>
     </>
     );
