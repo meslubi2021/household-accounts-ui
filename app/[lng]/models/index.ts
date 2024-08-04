9c7ac1cc2326ef686827b4b8f492e01c3bf07946
Jehyun Jung <congmul@hotmail.com>
2024-08-03 17:09:59 -0700

    Update User model

--- app/[lng]/models/index.ts
+++ app/[lng]/models/index.ts
@@ -2,10 +2,12 @@ import { Budget } from './budget';
 import { Transaction, TransactionType, AddTransactionPayload, PatchTransactionPayload, FixedExpenseType } from './transaction';
 import { CalendarEvent } from './calendar-event';
 import { Category, CategoryPayload } from './category';
+import { User } from './user'
 
 export type { 
     Budget, 
     Transaction, TransactionType, AddTransactionPayload, PatchTransactionPayload, FixedExpenseType,
     CalendarEvent,
-    Category, CategoryPayload
+    Category, CategoryPayload,
+    User
 }
---
+++ app/[lng]/models/user.ts
@@ -0,0 +1,10 @@
+export type User = {
+    _id: string;
+    email: string
+    firstName: string
+    lastName: string
+    fullname: string
+    joinThrough: string
+    lastLogin: string
+    updateAt: string
+}
