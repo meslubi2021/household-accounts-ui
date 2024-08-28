bf22f9723001fe9d99c5c027ae2b48271af92643
Jehyun Jung <congmul@hotmail.com>
2024-08-27 22:30:33 -0700

    Fix issue on dropdown box

--- app/ui/shared-components/dropdown/dropdown.tsx
+++ app/ui/shared-components/dropdown/dropdown.tsx
@@ -30,7 +30,7 @@ export const Dropdown:React.FC<Dropdown> = ( {lng, className, defaultValue, item
 
       <MenuItems
         transition
-        className={`dropdown-list-box absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in`}
+        className={`max-h-60 overflow-y-auto dropdown-list-box absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in`}
       >
         <div className="py-1">
           {
