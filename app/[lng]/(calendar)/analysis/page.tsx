98b992268c3fb103bd910b7b8b390f64c7c36bb2
Jehyun Jung <congmul@hotmail.com>
2024-09-13 16:20:44 -0700

    Add calendar on analysis page

--- app/[lng]/(calendar)/analysis/page.tsx
+++ app/[lng]/(calendar)/analysis/page.tsx
@@ -1,12 +1,39 @@
 "use client";
 
 import { useTranslation } from '@/app/lib/i18n/client';
+import { useDispatch, useSelector } from 'react-redux';
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import { calendarActions, refreshActions } from '@/app/lib/redux';
+import { format } from 'date-fns'
 
 import React, { useState } from 'react';
 
 export default function Index({ params: { lng }} : any) {
     const { t } = useTranslation(lng, 'main');
+    const dispatch = useDispatch();
+    const { selectedDateStr } = useSelector((state:any) => state.calendar);
+
+
+    const handleDatesSet = (arg: any) => {      
+        const calendarApi = arg.view.calendar;
+        const currentDate = calendarApi.getDate();
+    
+        // Selected Month, need to store it globally.
+        dispatch(calendarActions.setSelectedDateStr(format(currentDate, 'yyyy-MM-dd')));
+    };
     return (<>
+    <div className="budget-page-wrapper">
+        <FullCalendar 
+            plugins={[dayGridPlugin]}
+            datesSet={handleDatesSet} // to handle pre / next on headerTool bar event.
+            headerToolbar={{
+                left: 'prev',
+                center: 'title today',
+                right: 'next'
+            }}
+         />
+    </div>
        Coming soon
     </>
     );
